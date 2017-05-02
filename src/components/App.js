0672ac96d0d396a005915085adfcf6c671c02d23
Jawadban <jawadbanga@gmail.com>
2017-05-01 19:19:17 -0700

    deploying on heroku

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react'; /* jshint unused:false */
+import React, { Component } from 'react'; // eslint-disable-line no-alert
 import './App.css';
-import GoogleMapStatic from './googleMapsComponents/CurrentLocationMap.js'; /* jshint unused:false */
+import GoogleMapStatic from './googleMapsComponents/CurrentLocationMap.js'; // eslint-disable-line no-alert
 import LogUserData from './renderComponents/LogUserData.js'
 import { Router, Route, Link, hashHistory } from 'react-router'
 import GoogleWholeRoute from './googleMapsComponents/WholeRoute.js'
